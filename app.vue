a5b8d6d4756fdb7089eb8bc4e9257b00ba82c042
Mihai Racoreanu <mihai.racoreanu@gmail.com>
2023-12-13 12:36:35 +0000

    initial commit

---
+++ app.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+  const url = useRequestURL()
+  const pathname = url.pathname.replace('/', '')
+</script>
+
+<template>
+  <p>URL is: {{ url }}</p>
+  <p>Path is: {{ pathname }}</p>
+</template>
